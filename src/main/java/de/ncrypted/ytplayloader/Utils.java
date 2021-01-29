6eb221561df11dd302f6c66add43042d9eeb70aa
ncryptedV1 <roman1504@web.de>
2021-01-29 15:52:00 +0100

    initial commit

---
+++ src/main/java/de/ncrypted/ytplayloader/Utils.java
@@ -0,0 +1,33 @@
+package de.ncrypted.ytplayloader;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @author ncrypted
+ */
+public class Utils {
+
+    public static void handleYTDLUpdateStream(Process proc) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(proc.getInputStream()))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.startsWith("Updating to version")) {
+                    Logger.log("Youtube-DL updated to version " + line.split(" ")[3]);
+                } else if (line.startsWith("youtube-dl is up-to-date")) {
+                    Logger.log("Youtube-dl is up-to-date " + line.split(" ")[3]);
+                }
+            }
+        }
+    }
+
+    public static void readErrorStream(Process proc) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(proc.getErrorStream()))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        }
+    }
+}
---
+++ src/main/java/de/ncrypted/ytplayloader/YTPlayloader.java
@@ -0,0 +1,64 @@
+package de.ncrypted.ytplayloader;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import lombok.Getter;
+
+import java.io.File;
+import java.net.URISyntaxException;
+
+public class YTPlayloader extends Application {
+
+    @Getter
+    private static YTPLController controller;
+    @Getter
+    private static String jarDir;
+
+    static {
+        try {
+            jarDir = new File(YTPlayloader.class.getProtectionDomain()
+                    .getCodeSource()
+                    .getLocation()
+                    .toURI())
+                    .getParentFile()
+                    .getPath();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/YTPLGui.fxml"));
+        Parent root = loader.load();
+        primaryStage.setTitle("Made with {} and <3 by Oliver Schirmer");
+        primaryStage.getIcons()
+                .add(new Image(YTPlayloader.class.getClassLoader().getResourceAsStream("blackyt_arrow.png")));
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+
+        controller = loader.getController();
+        controller.setStageAndSetup(primaryStage);
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void stop() throws Exception {
+        System.out.println("Stage is shutting down...");
+        controller.stop();
+        super.stop();
+    }
+
+    public static void runOnUIThread(Runnable runnable) {
+        Platform.runLater(runnable);
+    }
+}
